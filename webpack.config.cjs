062aeabdc7aaf5813f5b73f4329528ce5f7db951
lll-yyy-nnn <110609697+lll-yyy-nnn@users.noreply.github.com>
2023-06-17 10:52:15 +0800

    Update webpack.config.cjs

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -33,28 +33,28 @@ module.exports = {
     filename: 'bundle.js',
     clean: true,
   },
-  // resolve: {
-  //   extensions: ['.js'],
-  // },
-  // module: {
-  //   rules: [{
-  //     test: /\.js$/,
-  //     exclude: /node_modules/,
-  //     use: {
-  //       loader: 'babel-loader',
-  //       options: {
-  //         presets: [
-  //           [
-  //             '@babel/preset-env',
-  //             {
-  //               "targets": "> 0.25%, not dead",
-  //               "useBuiltIns": "usage",
-  //               "corejs": "3.8.3",
-  //             }
-  //           ]
-  //         ]
-  //       }
-  //     }
-  //   }]
-  // }
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                "targets": "> 0.25%, not dead",
+                "useBuiltIns": "usage",
+                "corejs": "3.8.3",
+              }
+            ]
+          ]
+        }
+      }
+    }]
+  }
 };
